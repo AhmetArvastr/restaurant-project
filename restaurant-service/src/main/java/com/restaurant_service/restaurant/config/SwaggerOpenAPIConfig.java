a182d570b23da98324ad477ed72d6bd17f6da567
unknown <ahmetarvastr@gmail.com>
2024-11-03 17:25:36 +0300

    adding configs

--- restaurant-service/src/main/java/com/restaurant_service/restaurant/config/SwaggerOpenAPIConfig.java
+++ restaurant-service/src/main/java/com/restaurant_service/restaurant/config/SwaggerOpenAPIConfig.java
@@ -1,9 +1,12 @@
-package com.seyidahmetarvas.userservice.config;
+package com.restaurant_service.restaurant.config;
 
 import io.swagger.v3.oas.annotations.ExternalDocumentation;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Contact;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import io.swagger.v3.oas.annotations.servers.Server;
 
 @OpenAPIDefinition(
@@ -17,8 +20,16 @@ import io.swagger.v3.oas.annotations.servers.Server;
                 description = "Source code on GitHub",
                 url = "https://www.github.com/AhmetArvastr/restaurant-project"
         ),
-        servers = {@Server(description = "user-service", url = "http://localhost:8083")}
+        servers = {@Server(description = "restaurant-service", url = "http://localhost:8081")}
 
+)
+@SecurityScheme(
+        name = "bearerAuth",
+        description = "JWT authentication",
+        scheme = "bearer",
+        type = SecuritySchemeType.HTTP,
+        bearerFormat = "JWT",
+        in = SecuritySchemeIn.HEADER
 )
 public class SwaggerOpenAPIConfig {
 }
