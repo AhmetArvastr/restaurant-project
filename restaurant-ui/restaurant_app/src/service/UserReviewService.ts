33a521365bb65b6cca56fdd203176805c6583a9f
unknown <ahmetarvastr@gmail.com>
2025-01-17 18:23:25 +0300

    feat(restaurant-ui): Create React project and implement API service, custom hooks, and data fetching components

---
+++ restaurant-ui/restaurant_app/src/service/UserReviewService.ts
@@ -0,0 +1,80 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { axiosInstance } from "./AxiosInstance";
+
+interface UserReviewApiResponse {
+  data: {
+    id: number;
+    user: {
+      baseAdditionalFields: {
+        createdAt: string;
+        updatedAt: string;
+        creatorId: number;
+        updatedId: number;
+      };
+      id: number;
+      name: string;
+      surname: string;
+      birthDate: string;
+      email: string;
+      gender: string;
+      userStatus: string;
+      latitude: number;
+      longitude: number;
+    };
+    restaurantId: string;
+    restaurantName: string;
+    comment: string;
+    userRate: number;
+  }[];
+  responseDate: string;
+  message: string;
+  success: boolean;
+}
+
+interface UserReviewPost{
+  "restaurantId": string,
+  "comment": string,
+  "userRate": number,
+  "user": {
+    "baseAdditionalFields": {
+      "createdAt": string,
+      "updatedAt": string,
+      "creatorId": number,
+      "updatedId": number
+    },
+    "id": number,
+    "name": string,
+    "surname": string,
+    "birthDate": string,
+    "email": string,
+    "gender": string,
+    "userStatus": string,
+    "latitude": number,
+    "longitude": number
+  }
+}
+
+const fetchUserReviewByRestaurantId = async (id: string): Promise<any> => {
+    try {
+        const response = await axiosInstance.get<any>(`/api/v1/users/reviews/restaurant/${id}`);
+        return response.data;
+    } catch (e) {
+        console.log(e);
+        throw e;
+    }
+}
+
+const saveUserReview = async (userReview: UserReviewPost): Promise<any> => {
+    try {
+        const response = await axiosInstance.post<UserReviewPost>("/api/v1/users/reviews", userReview);
+        console.log(response.data);
+        return response.data;
+    } catch (e) {
+        console.log(e);
+        throw e;
+    }
+
+}
+
+export { fetchUserReviewByRestaurantId,saveUserReview}
+
---
+++ restaurant-ui/restaurant_app/src/service/UserService.ts
@@ -0,0 +1,41 @@
+import { axiosInstance } from "./AxiosInstance";
+
+interface User {
+    "name": string,
+    "surname": string,
+    "birthDate": string,
+    "email": string,
+    "gender": string,
+    "userStatus": string,
+    "latitude": number,
+    "longitude": number
+}
+
+const deleteUser = async (id: number) => {
+    try {
+        const response = await axiosInstance.delete(`/api/v1/users/${id}`);
+        return response.data;
+    }catch (error) {
+        console.error(error);
+    }
+}
+
+const fetchAllUsers = async () => {
+    try {
+        const response = await axiosInstance.get(`/api/v1/users`);
+        return response.data;
+    }catch (error) {
+        console.error(error);
+    }
+}
+
+const saveUser = async (user: User) => {
+    try {
+        const response = await axiosInstance.post(`/api/v1/users`, user);
+        return response.data;
+    }catch (error) {
+        console.error(error);
+    }
+}
+
+export {deleteUser,fetchAllUsers,saveUser}
