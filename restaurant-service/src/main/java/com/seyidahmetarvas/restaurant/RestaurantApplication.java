8d7993b44676a3725c392f2d3d8a4fd02a854e52
unknown <ahmetarvastr@gmail.com>
2025-02-04 20:13:08 +0300

    feat(restaurant-service):Ensured data is indexed automatically when the application starts

--- restaurant-service/src/main/java/com/seyidahmetarvas/restaurant/RestaurantApplication.java
+++ restaurant-service/src/main/java/com/seyidahmetarvas/restaurant/RestaurantApplication.java
@@ -1,8 +1,15 @@
 package com.seyidahmetarvas.restaurant;
 
+import com.seyidahmetarvas.restaurant.model.Restaurant;
+import com.seyidahmetarvas.restaurant.repository.RestaurantRepository;
+import com.seyidahmetarvas.restaurant.util.JsonUtil;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+
+import java.util.List;
 
 @SpringBootApplication
 @EnableFeignClients
@@ -10,5 +17,15 @@ public class RestaurantApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RestaurantApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner runner(RestaurantRepository restaurantRepository) {
+		return args -> {
+			if (restaurantRepository.count() == 0) {
+				List<Restaurant> users = JsonUtil.readRestaurantsFromJson();
+				restaurantRepository.saveAll(users);
+			}
+		};
 	}
 }
--- restaurant-service/src/main/java/com/seyidahmetarvas/restaurant/util/JsonUtil.java
+++ restaurant-service/src/main/java/com/seyidahmetarvas/restaurant/util/JsonUtil.java
@@ -3,6 +3,7 @@ package com.seyidahmetarvas.restaurant.util;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.seyidahmetarvas.restaurant.model.Restaurant;
+import org.springframework.core.io.ClassPathResource;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -11,10 +12,9 @@ import java.util.List;
 public class JsonUtil {
     public static List<Restaurant> readRestaurantsFromJson() throws IOException {
         ObjectMapper objectMapper = new ObjectMapper();
-        TypeReference<List<Restaurant>> typeReference = new TypeReference<>() {
-        };
-        InputStream inputStream = TypeReference.class.getResourceAsStream("/restaurants.json");
-        return objectMapper.readValue(inputStream, typeReference);
-
+        ClassPathResource resource = new ClassPathResource("data/restaurants.json");
+        InputStream inputStream = resource.getInputStream();
+        return objectMapper.readValue(inputStream, new TypeReference<List<Restaurant>>() {
+        });
     }
 }
